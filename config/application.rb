52187b7c79aa4a640e8bc8be2810433ef0655fc2
Muneeb Meer <meer_online@yahoo.com>
2012-08-31 14:37:47 +0500

    User Signup Complete

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+   Bundler.require(:default, Rails.env) if defined?(Bundler)
 end
 
 module SampleApp
@@ -64,5 +64,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
   end
 end
